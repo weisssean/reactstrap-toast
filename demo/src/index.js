e1cdd04882a9b8e2855e5620ca0f2446c60183b1
weisssean <weisssean@gmail.com>
2019-09-13 10:17:42 -0400

    z index

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  Toaster  from '../../src/index'
 
 import Example from '../../src'
 
 class Demo extends Component {
+
+  componentDidMount(){
+    this.toaster.showToast("success","Success", "Showing toaster message");
+  }
   render() {
     return <div>
       <h1>reactstrap-toast Demo</h1>
-      <Example/>
+      <Toaster ref={(ref)=>{this.toaster = ref}}/>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import React, {Component} from 'react'
+import React from "react";
+import { Toast, ToastBody, ToastHeader } from "reactstrap";
 
-export default class extends Component {
+class Toaster extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.show && !prevState.show) {
+      setTimeout(() => {
+        this.setState({ show: false });
+      }, 5000);
+    }
+  }
+
+  showToast = (style, title, message) => {
+    this.setState({ show: true, mssstyle: style, title, message });
+  };
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <div style={{ position: "fixed", top: 25, right: 25, zIndex:99 }}>
+        <Toast isOpen={this.state.show}>
+          <ToastHeader icon={this.state.mssstyle}>
+            {this.state.title}
+          </ToastHeader>
+          <ToastBody>{this.state.message}</ToastBody>
+        </Toast>
+      </div>
+    );
   }
 }
+
+export default Toaster;
